9457fe0bf45e50f98f48432b6c4dcb1ca66c642b
Sahar Far <saharmfar@gmail.com>
2014-05-28 10:41:33 -0400

    .js and .html file added

---
+++ main.js
@@ -0,0 +1,18 @@
+var red = [0, 100, 63];
+var orange = [59, 100, 80];
+var green = [90, 200, 40];
+var blue = [196, 77, 55];
+var purple = [300, 50, 60];
+
+var myName = "Sahar";
+var letterColors=[red, orange, green, purple, blue]
+
+if(true){
+    bubbleShape = "circle"
+}
+else{
+    bubbleShape = "square"
+}
+
+drawName(myName, letterColors);
+bounceBubbles()
